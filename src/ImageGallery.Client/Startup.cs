257764064c1f8ec9134805e35d1c0e72be1c1acb
Qing Ma <q.ma@ncontrol.cloud>
2021-03-06 13:25:45 +0100

    GetClaimsFromUserInfoEndpoint

--- src/ImageGallery.Client/Startup.cs
+++ src/ImageGallery.Client/Startup.cs
@@ -52,6 +52,7 @@ namespace ImageGallery.Client
                 options.Scope.Add("profile");
                 options.SaveTokens = true;
                 options.ClientSecret = "secret";
+                options.GetClaimsFromUserInfoEndpoint = true;
             });
         }
 
